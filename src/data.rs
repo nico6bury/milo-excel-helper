012d338852babe547ca43719d94de14d35b30d8a
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2024-07-09 15:15:08 -0500

    Suspected sample ordering is now automatically guess at for InputFiles.

--- src/data.rs
+++ src/data.rs
@@ -8,11 +8,34 @@ pub struct InputLine {
 	pub perc_area2: f32,
 }
 
+#[derive(Clone, Copy, PartialEq, PartialOrd, Debug)]
+pub enum SampleOrder {
+	AB51,
+	BA15,
+	Unknown,
+}//end enum SampleOrder
+
+impl SampleOrder {
+	pub fn from_file_id(file_id: &str) -> SampleOrder {
+		let file_components: Vec<&str> = file_id.split('-').collect();
+		if file_components.contains(&"up") || file_components.contains(&"uc") {SampleOrder::AB51}
+		else if file_components.contains(&"dn") || file_components.contains(&"dc") {SampleOrder::BA15}
+		else {SampleOrder::Unknown}
+	}//end from_file_id
+}//end impl for SampleOrder
+
 #[derive(Clone,PartialEq,PartialOrd,Debug)]
 pub struct InputFile {
 	pub file_id: String,
 	pub input_lines: Vec<InputLine>,
-}
+	pub sample_ordering: SampleOrder,
+}//end struct InputFile
+
+impl InputFile {
+	pub fn new(file_id: &str, input_lines: Vec<InputLine>) -> InputFile {
+		InputFile {file_id: file_id.to_string(), input_lines, sample_ordering: SampleOrder::AB51}
+	}//end new()
+}//end impl for InputFile
 
 pub fn read_csv_file(file: &PathBuf) -> Option<Vec<InputFile>> {
 	let mut input_files: Vec<InputFile> = Vec::new();
@@ -56,10 +79,7 @@ pub fn read_csv_file(file: &PathBuf) -> Option<Vec<InputFile>> {
 		let new_input_line = InputLine {grid_idx,area1,area2,perc_area2,};
 		// make sure we're separating files from lines
 		if !file_id.eq(&last_file_id) {
-			let new_input_file = InputFile {
-				file_id: last_file_id.to_string(),
-				input_lines: tmp_row_data.clone(),
-			};
+			let new_input_file = InputFile::new(&last_file_id, tmp_row_data);
 			input_files.push(new_input_file);
 			tmp_row_data = vec![new_input_line];
 			last_file_id = file_id.to_string();
@@ -70,10 +90,7 @@ pub fn read_csv_file(file: &PathBuf) -> Option<Vec<InputFile>> {
 	
 	// clean up anything left and add to returned Vec
 	if tmp_row_data.len() > 0 {
-		let new_input_file = InputFile {
-			file_id: last_file_id.to_string(),
-			input_lines: tmp_row_data,
-		};
+		let new_input_file = InputFile::new(&last_file_id,tmp_row_data);
 		input_files.push(new_input_file);
 	}//end if we should add the last few lines to input_files
 
