f969635dd25922c97827265617b8ec8579dcdfbd
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2024-07-09 13:57:30 -0500

    Added ability to read in all the relevant data from a csv.

---
+++ src/data.rs
@@ -0,0 +1,81 @@
+use std::{fs::{self}, path::PathBuf};
+
+#[derive(Clone, Copy,PartialEq, PartialOrd,Debug,Default)]
+pub struct InputLine {
+	pub grid_idx: i32,
+	pub area1: i32,
+	pub area2: i32,
+	pub perc_area2: f32,
+}
+
+#[derive(Clone,PartialEq,PartialOrd,Debug)]
+pub struct InputFile {
+	pub file_id: String,
+	pub input_lines: Vec<InputLine>,
+}
+
+pub fn read_csv_file(file: &PathBuf) -> Option<Vec<InputFile>> {
+	let mut input_files: Vec<InputFile> = Vec::new();
+	let mut last_file_id;
+	let mut tmp_row_data = Vec::new();
+
+	let mut _headers: Vec<&str> = Vec::new();
+	let mut header_idx = 0;
+
+	let contents = fs::read_to_string(file).unwrap();
+	let lines: Vec<&str> = contents.split('\n').collect();
+	
+	// get the headers and header_idx
+	loop {
+		let line = lines[header_idx];
+		let cols: Vec<&str> = line.split(',').collect();
+		if cols.len() > 5 {
+			// save headers
+			_headers = cols;
+			// figure out the first file_id
+			let n_line = lines[header_idx + 1];
+			let n_cols: Vec<&str> = n_line.split(',').collect();
+			last_file_id = n_cols[0].to_string();
+			// exit loop, work here is done
+			break;
+		} else {header_idx += 1;}
+	}//end looping to find headers
+
+	// loop over lines after headers, get the data
+	for i in (header_idx+1)..lines.len() {
+		// just get the actual columns
+		let line = lines[i];
+		let cols: Vec<&str> = line.split(',').collect();
+		if cols.len() < 5 {println!("{:?}",cols); continue;}
+		// get all the actual data
+		let file_id = cols[0];
+		let grid_idx: i32 = cols[1].parse().unwrap_or(-2);
+		let area1: i32 = cols[2].parse().unwrap_or(-2.) as i32;
+		let area2: i32 = cols[3].parse().unwrap_or(-2.) as i32;
+		let perc_area2: f32 = cols[4].parse().unwrap_or(-2.);
+		let new_input_line = InputLine {grid_idx,area1,area2,perc_area2,};
+		// make sure we're separating files from lines
+		if !file_id.eq(&last_file_id) {
+			let new_input_file = InputFile {
+				file_id: last_file_id.to_string(),
+				input_lines: tmp_row_data.clone(),
+			};
+			input_files.push(new_input_file);
+			tmp_row_data = vec![new_input_line];
+			last_file_id = file_id.to_string();
+		} else {
+			tmp_row_data.push(new_input_line);
+		}//end else we add a new line as usual
+	}//end looping over indices for lines
+	
+	// clean up anything left and add to returned Vec
+	if tmp_row_data.len() > 0 {
+		let new_input_file = InputFile {
+			file_id: last_file_id.to_string(),
+			input_lines: tmp_row_data,
+		};
+		input_files.push(new_input_file);
+	}//end if we should add the last few lines to input_files
+
+	return Some(input_files);
+}//end read_csv_file(reader)
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 use gui::GUI;
-use rust_excel_helper::data::*;
+use rust_excel_helper::data::{self};
 
 mod gui;
 
 fn main() {
-	let gui = GUI::initialize();
+	let mut gui = GUI::initialize();
 	let recv = gui.get_receiver();
 
 	while gui.wait() {
 		if let Some(msg) = recv.recv() {
 			match msg {
 				gui::InterfaceMessage::CSVInputFile(input_file) => {
-					println!("I'll get to processing {:?} later", input_file);
+					gui.start_wait();
+					
+					let data = data::read_csv_file(&input_file).unwrap();
+					for dat in data {
+						println!("\nFileID {}", dat.file_id);
+						for line in dat.input_lines {
+							println!("{:?}", line);
+						}
+					}
+
+					gui.end_wait();
 				},
 				gui::InterfaceMessage::AppClosing => GUI::quit(),
 				_ => println!("Message {:?} not recognized or supported.", msg),
