313cd9dc91e2ee351d379b702d682353fdd75dc1
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2024-07-09 10:07:27 -0500

    Create and run GUI from main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use gui::GUI;
+use rust_excel_helper::data::*;
+
+mod gui;
+
 fn main() {
-    println!("Hello, world!");
-}
+	let gui = GUI::initialize();
+	let recv = gui.get_receiver();
+
+	while gui.wait() {
+		if let Some(msg) = recv.recv() {
+			match msg {
+				gui::InterfaceMessage::CSVInputFile(input_file) => {
+					println!("I'll get to processing {:?} later", input_file);
+				},
+				gui::InterfaceMessage::AppClosing => GUI::quit(),
+				_ => println!("Message {:?} not recognized or supported.", msg),
+			}//end matching based on the message
+		}//end if we have an Interface Message
+	}//end main app loop
+}//end main method
