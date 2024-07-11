6cb58ae0ec77d4d205c1a884599dfefe72421d42
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2024-07-11 14:40:23 -0500

    Integrated the sum sheet into the program, extracted from extract_sum_chunk()

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,7 @@ fn main() {
 					let labelled_chunks = excel::extract_labelled_chunks(&data);
 					let sorted_1_chunks = excel::extract_sorted_chunks_1(&data);
 					let sorted_2_chunks = excel::extract_sorted_chunks_2(&data);
+					let sum_chunk = excel::extract_sum_chunk(&data);
 					let process_duration = process_start.elapsed();
 
 					// write all the data chunks to various excel sheets
@@ -62,6 +63,12 @@ fn main() {
 						sorted_2_chunks.iter(),
 						"sorted-2"
 					).unwrap_or_else(|_| println!("Failed to write sorted_2_chunks."));
+
+					excel::write_chunks_to_sheet(
+						&mut wb,
+						vec![sum_chunk].iter(),
+						"sum"
+					).unwrap_or_else(|_| println!("Failed to write sum chunk."));
 
 					if let Ok(worksheet) = wb.worksheet_from_index(2) {worksheet.set_active(true);}
 
