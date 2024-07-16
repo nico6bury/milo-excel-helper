fb6478c330180d1c7613fbafdaf0585ed2877c2f
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2024-07-16 13:23:40 -0500

    Reorganized chunk extraction and file writing in main.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use gui::GUI;
+use rust_xlsxwriter::{Workbook, XlsxError};
 use std::time::{Duration, Instant};
-use milo_excel_helper::{data::{self}, excel};
+use milo_excel_helper::{data::{self, InputFile}, excel::{self, DataChunk}};
 
 mod gui;
 
@@ -35,11 +36,8 @@ fn main() {
 					// do a bunch of processing on data to get data chunks
 					println!("Ready to start extracting data chunks from the data we read!");
 					let process_start = Instant::now();
-					let labelled_chunks = excel::extract_labelled_chunks(&data);
-					let sorted_1_chunks = excel::extract_sorted_chunks_1(&data);
-					let sorted_2_chunks = excel::extract_sorted_chunks_2(&data);
-					let sum_chunk = excel::extract_sum_chunk(&data);
-					let stats_chunk = excel::extract_stats_chunk(&data);
+					let detail_chunks = get_detail_chunks(&data);
+					let sum_chunks = get_sum_chunks(&data);
 					let process_duration = process_start.elapsed();
 
 					// write all the data chunks to various excel sheets
@@ -47,36 +45,10 @@ fn main() {
 					let workbook_start = Instant::now();
 
 					println!("Writing data chunks to sheets!");
-					excel::write_chunks_to_sheet(
-						&mut wb,
-						labelled_chunks.iter(),
-						"labelled"
-					).unwrap_or_else(|_|println!("Failed to write labelled chunks."));
-
-					excel::write_chunks_to_sheet(
-						&mut wb,
-						sorted_1_chunks.iter(),
-						"sorted-1"
-					).unwrap_or_else(|_|println!("Failed to write sorted_1_chunks."));
-
-					excel::write_chunks_to_sheet(
-						&mut wb,
-						sorted_2_chunks.iter(),
-						"sorted-2"
-					).unwrap_or_else(|_| println!("Failed to write sorted_2_chunks."));
-
-					excel::write_chunks_to_sheet(
-						&mut wb,
-						vec![sum_chunk].iter(),
-						"sum"
-					).unwrap_or_else(|_| println!("Failed to write sum chunk."));
-
-					excel::write_chunks_to_sheet(
-						&mut wb,
-						vec![stats_chunk].iter(),
-						"stats"
-					).unwrap_or_else(|_| println!("Failed to write stats chunk."));
-
+					write_detail_chunks(&mut wb, detail_chunks)
+						.unwrap_or_else(|_| println!("Failed to write some detailed chunks"));
+					write_sum_chunks(&mut wb, sum_chunks)
+						.unwrap_or_else(|_| println!("Failed to write some sum chunks"));
 					if let Ok(worksheet) = wb.worksheet_from_index(4) {worksheet.set_active(true);}
 
 					// figure out the output path we want for the xlsx file
@@ -116,3 +88,90 @@ fn format_milliseconds(duration: Duration) -> String {
 		Ok(micros) => format!("{0:.2}", micros / 1000.),
 	}//end matching whether we can parse float-micros
 }//end format_milliseconds(duration)
+
+/// Shorthand for extracting:
+/// - labelled_chunks
+/// - sorted_1_chunks
+/// - sorted_2_chunks
+fn get_detail_chunks(
+	data: &Vec<InputFile>
+) -> (Vec<DataChunk>,Vec<DataChunk>,Vec<DataChunk>) {
+	let labelled_chunks = excel::extract_labelled_chunks(&data);
+	let sorted_1_chunks = excel::extract_sorted_chunks_1(&data);
+	let sorted_2_chunks = excel::extract_sorted_chunks_2(&data);
+	(labelled_chunks, sorted_1_chunks, sorted_2_chunks)
+}//end get_detail_chunks
+
+/// Shorthand for extracting:
+/// - sum_chunk
+/// - stats_chunk
+fn get_sum_chunks(
+	data: &Vec<InputFile>
+) -> (DataChunk, DataChunk) {
+	let sum_chunk = excel::extract_sum_chunk(data);
+	let stats_chunk = excel::extract_stats_chunk(data);
+	(sum_chunk, stats_chunk)
+}//end get_sum_chunks
+
+/// Shorthand for writing
+/// - labelled chunks
+/// - sorted-1 chunks
+/// - sorted-2 chunks
+fn write_detail_chunks(
+	workbook: &mut Workbook,
+	detail_chunks: (Vec<DataChunk>,Vec<DataChunk>,Vec<DataChunk>),
+) -> Result<(), XlsxError> {
+	let mut found_err = Ok(());
+	excel::write_chunks_to_sheet(
+		workbook,
+		detail_chunks.0.iter(),
+		"labelled"
+	).unwrap_or_else(|err| {
+		found_err = Err(err);
+		println!("Failed to write labelled chunks.")
+	});
+	excel::write_chunks_to_sheet(
+		workbook,
+		detail_chunks.1.iter(),
+		"sorted-1"
+	).unwrap_or_else(|err| {
+		found_err = Err(err);
+		println!("Failed to write labelled chunks.")
+	});
+	excel::write_chunks_to_sheet(
+		workbook,
+		detail_chunks.2.iter(),
+		"sorted-2"
+	).unwrap_or_else(|err| {
+		found_err = Err(err);
+		println!("Failed to write labelled chunks.")
+	});
+	return found_err;
+}//end write_detail_chunks()
+
+/// Shorthand for writing
+/// - sum chunk
+/// - stats chunk
+fn write_sum_chunks(
+	workbook: &mut Workbook,
+	sum_chunks: (DataChunk, DataChunk),
+) -> Result<(),XlsxError> {
+	let mut found_err = Ok(());
+	excel::write_chunks_to_sheet(
+		workbook,
+		vec![sum_chunks.0].iter(),
+		"sum"
+	).unwrap_or_else(|err| {
+		found_err = Err(err);
+		println!("Failed to write labelled chunks.")
+	});
+	excel::write_chunks_to_sheet(
+		workbook,
+		vec![sum_chunks.1].iter(),
+		"stats"
+	).unwrap_or_else(|err| {
+		found_err = Err(err);
+		println!("Failed to write labelled chunks.")
+	});
+	return found_err;
+}
